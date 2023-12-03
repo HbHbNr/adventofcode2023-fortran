a0b26d8351bca523f3a37aef77ffc771b79c0d0f
Sebastian <hbhbnr@posteo.net>
2023-12-03 18:13:37 +0100

    Improve script for creating files for new days

--- newday.sh
+++ newday.sh
@@ -12,6 +12,11 @@ make src/day${day}.f90
 make src/day${day}_main.f90
 make src/day${day}_test.f90
 make fruitpy/day${day}.py
-touch inputfiles/day${day%?}_example.txt
-touch inputfiles/day${day%?}_input.txt
+for part in example input; do
+    if [ ! -f inputfiles/day${day%?}_${part}.txt ]; then
+        touch inputfiles/day${day%?}_${part}.txt
+    else
+        echo "inputfiles/day${day%?}_${part}.txt already exists."
+    fi
+done
 ls -l inputfiles/day${day%?}_*.txt
